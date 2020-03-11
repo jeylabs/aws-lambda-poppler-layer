8e3d5a12a3c3ec640b8b821cda6ad78fdc4b6bbf
Pavinthan <pavinthan@outlook.com>
2020-03-11 10:43:06 +0530

    Publish artifacts

--- export.sh
+++ export.sh
@@ -9,6 +9,7 @@ cp -R /runtime/* /opt/
 
 ls -la
 
+sed -i '/<!-- Font directory list -->/a <dir>/tmp/fonts</dir>' /opt/etc/fonts/fonts.conf
 sed -i '/<!-- Font directory list -->/a <dir>/opt/share/fonts</dir>' /opt/etc/fonts/fonts.conf
 
 zip --quiet --recurse-paths /export/${ZIP_FILE_NAME}.zip .
